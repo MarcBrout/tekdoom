d009fa064323643aa62f16174015c3bb1ca3209c
maud.marel@epitech.eu <maud.marel@epitech.eu>
2016-01-25 15:02:31 +0100

    début assemblage avec interface

--- wolf_to_the_rescue/include/interface.h
+++ wolf_to_the_rescue/include/interface.h
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed Dec 16 17:10:21 2015 maud marel
-** Last update Mon Jan 25 13:54:04 2016 maud marel
+** Last update Mon Jan 25 14:31:31 2016 maud marel
 */
 
 #ifndef INTERFACE_H_
@@ -60,31 +60,31 @@ t_bunny_response	my_escape(t_bunny_event_state,
 void			tekpixel(t_bunny_pixelarray *,
 				 t_bunny_position *,
 				 unsigned int);
-void			tekpixel2(t_bunny_pixelarray *,
-				 t_bunny_position *,
-				 t_color *);
+/* void			tekpixel2(t_bunny_pixelarray *, */
+/* 				 t_bunny_position *, */
+/* 				 t_color *); */
 void			my_set_square(t_bunny_pixelarray *,
 				      unsigned int);
-void			draw_life_bar(t_interface *);
-int			draw_move_life(t_interface *);
-int			draw_move_life2(t_interface *, t_bunny_position);
-void			draw_square_life(t_interface *);
-void			action_draw_square_life(t_interface *,
-						    t_bunny_position,
-						    t_bunny_position);
-void			interface(t_interface *);
-void			interface_init(t_interface *);
-int			draw_heart(t_interface *);
-void			draw_heart2(t_interface *, t_bunny_position,
-				    t_bunny_position, int);
-void			draw_heart_else(t_interface *, int, int);
-int			draw_bullet(t_interface *);
-void			draw_bullet2(t_interface *, t_bunny_position,
-				    t_bunny_position, int);
-void			draw_bullet_else(t_interface *, int, int);
-int			draw_game_over(t_interface *);
-int			draw_last_chance(t_interface *);
-int			draw_try_again(t_interface *);
-int			load_picture(t_interface *);
+/* void			draw_life_bar(t_param *); */
+/* int			draw_move_life(t_param *); */
+/* int			draw_move_life2(t_param *, t_bunny_position); */
+/* void			draw_square_life(t_param *); */
+/* void			action_draw_square_life(t_param *, */
+/* 						    t_bunny_position, */
+/* 						    t_bunny_position); */
+/* void			interface(t_param *); */
+/* void			interface_init(t_param *); */
+/* int			draw_heart(t_param *); */
+/* void			draw_heart2(t_param *, t_bunny_position, */
+/* 				    t_bunny_position, int); */
+/* void			draw_heart_else(t_param *, int, int); */
+/* int			draw_bullet(t_param *); */
+/* void			draw_bullet2(t_param *, t_bunny_position, */
+/* 				    t_bunny_position, int); */
+/* void			draw_bullet_else(t_param *, int, int); */
+/* int			draw_game_over(t_param *); */
+/* int			draw_last_chance(t_param *); */
+/* int			draw_try_again(t_param *); */
+/* int			load_picture(t_param *); */
 
 #endif /* !INTERFACE_H_ */
