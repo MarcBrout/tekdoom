01b122990b16e394a3ef0bae18dccdd5fbb72f18
benjamin duhieu <benjamin.duhieu@epitech.eu>
2016-01-27 02:58:51 +0100

    euh ...

---
+++ wolf_to_the_rescue/src/clik.c
@@ -0,0 +1,44 @@
+/*
+** cliK.c for clik in
+**
+** Made by benjamin duhieu
+** Login   <duhieu_b@epitech.net>
+**
+** Started on  Wed Jan 27 00:48:48 2016 benjamin duhieu
+** Last update Wed Jan 27 02:05:39 2016 benjamin duhieu
+*/
+
+#include "wolf.h"
+
+t_bunny_response		main_click(UNUSED t_bunny_event_state state,
+					   UNUSED t_bunny_mousebutton button,
+					   void	*data)
+{
+  const bool			*mouse;
+  t_param			*doom;
+  int				cac;
+
+  dool = data;
+  mouse = bunny_get_mouse_button();
+  if (mouse[BMB_LEFT])
+    {
+      draw_weapon(doom, doom->inter.gun.i);
+      if (doom->inter.gun.i == 0)
+	bunny_sound_play(doom->sound->shotgun);
+      if (doom->inter.gun.i == 1)
+	bunny_sound_play(doom->sound->knife);
+      if (doom->inter.gun.i == 2)
+	bunny_sound_play(doom->sound->bazook);
+      if (doom->inter.gun.i == 3)
+	bunny_sound_play(doom->sound->sulf);
+      if (doom->inter.gun.i == 4)
+	{
+	  cac = rand() % 2;
+	  if (!cac)
+	    bunny_sound_play(doom->sound->cac);
+	  else
+	    bunny_sound_play(doom->sound->cac2);
+	}
+    }
+  return (GO_ON);
+}
